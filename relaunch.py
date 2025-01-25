1273de6ef544879ff898dc3ae58334e159659c21
prytlubomir <prytlubomyr@gmail.com>
2025-01-25 17:49:13 +0000

    v1.0.0 Relaunch hardcoded command.

---
+++ relaunch.py
@@ -0,0 +1,76 @@
+'''
+Relaunch python process by its command.
+'''
+
+from typing import Iterable
+import subprocess as sub
+import sys
+
+
+def get_pid(process_name: str) -> int:
+    '''
+    get_pid(process_name: str) -> int:
+
+    Get the PID of a process using "pgrep" command.
+    '''
+    # pgrep - displays processes selected by regex
+    # -f - use full process name to match
+    cmd = ['pgrep', '-f', process_name]
+
+    proc = sub.Popen(cmd, stdout=sub.PIPE)
+
+    result = proc.communicate()[0]
+    result = result.decode().strip()
+    #print(result.__repr__())
+
+    if not result:
+        raise ValueError(f'Process name {process_name} not found.')
+
+    return int(result)
+
+
+def kill_process(pid: int | str) -> bool:
+    '''
+    kill_process(pid: int | str) -> bool:
+
+    Stops a process with a specified PID.
+
+    Return:
+        *  0 | False - success (no error)
+        *  1 | True  - fail (wrong PID)
+    '''
+    cmd = ['kill', str(pid)]
+
+    proc = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
+
+    err = proc.communicate()[1]
+
+    if err:
+        return 1
+    return 0
+
+
+def launch_process(command: str | Iterable) -> int:
+    '''Run a command in an independent process.'''
+    cmd = command
+    if isinstance(cmd, str):
+        cmd = cmd.split()
+
+    proc = sub.Popen(cmd, close_fds = True)
+    pid = proc.pid
+
+    return pid
+
+
+def main():
+    '''Relaunch process'''
+    command = 'python3.11 server.py'
+    pid = get_pid(command)
+    kill_process(pid)
+    new_pid = launch_process(command)
+
+    print(new_pid)
+
+
+if __name__ == "__main__":
+    main()
